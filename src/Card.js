21f2379524f687186ef2784dfdc9890f0506c00b
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-04-27 16:58:39 -0500

    Add Card component

---
+++ src/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+ const Card = ({name, email, id}) => {
+
+   return (
+     <div className='bg-light-green dib br3 pa3 ma2 grow tc bw2 shadow-5'>
+       <img alt='robot' src={`https://robohash.org/${id}?200x200`}/>
+       <div>
+
+         <h2>{name}</h2>
+         <p>{email}</p>
+       </div>
+
+     </div>
+
+
+   );
+
+
+ }
+
+ export default Card;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'tachyons';
 import './index.css';
 import App from './App';
+import Card from './Card';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Card id="1" name="Emm" email="emm@test.com"/>
+    <Card id="2" name="Emm" email="emm@test.com"/>
+    <Card id="3" name="Emm" email="emm@test.com"/>
+  </div>
+  , document.getElementById('root'));
 registerServiceWorker();
