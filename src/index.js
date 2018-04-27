7ccbc83eb36f5b0e945864e07a3e04c8add6f298
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-04-27 17:20:24 -0500

    Add robots.js and CardList component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'tachyons';
-import './index.css';
-import App from './App';
-import Card from './Card';
-
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+import './index.css';
+
+import App from './App';
+import CardList from './CardList';
+import {robots} from './robots'
+
 
 ReactDOM.render(
-  <div>
-    <Card id="1" name="Emm" email="emm@test.com"/>
-    <Card id="2" name="Emm" email="emm@test.com"/>
-    <Card id="3" name="Emm" email="emm@test.com"/>
-  </div>
+  <CardList robots={robots}/>
   , document.getElementById('root'));
 registerServiceWorker();
