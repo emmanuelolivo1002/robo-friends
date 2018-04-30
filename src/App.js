bd3aa2e6aec111f15d95ef5355793753dec263b8
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-04-30 13:28:01 -0500

    Remove robots.js and Add placeholder/users API

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React, {Component} from 'react';
 import './App.css';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
-
-
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-        robots: robots,
-        searchfield: ''
+      robots: [],
+      searchfield: ''
     }
   }
 
@@ -19,17 +16,26 @@ class App extends Component {
     this.setState({searchfield: event.target.value});
   }
 
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({robots: users}));
+
+  }
+
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-      <div className='tc'>
+
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>
+    } else {
+      return (<div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots= {filteredRobots} />
-      </div>
-    );
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
+      </div>);
+    }
   }
 }
 
